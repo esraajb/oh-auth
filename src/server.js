bc46c3e1842cae12fd6d8a9553a7613d8f4f4c53
lucymonie <lucy.monie.hall@gmail.com>
2016-12-07 14:45:57 +0000

    adds basic server to project

--- src/server.js
+++ src/server.js
@@ -2,8 +2,18 @@ const Hapi = require('hapi');
 const Inert = require('inert');
 const env = require('env2')('../config.env');
 const Vision = require('vision');
-const routes = require('./routes.js');
+// const routes = require('./routes.js');
 const server = new Hapi.Server();
 
+server.connection({
+  port: process.env.PORT || 4000
+});
+
+server.register([Inert,Vision],
+  (err) => {
+    if(err) throw err;
+    // server.route(routes);
+  }
+)
 
 module.exports = server;

a743eccafa3ad186c90aab7e391225a0ddd1a6ce
denesnori <denes.nori@yahoo.com>
2016-12-09 10:46:04 +0000

    add lucy to db

--- src/db_build.sql
+++ src/db_build.sql
@@ -13,10 +13,10 @@ CREATE TABLE IF NOT EXISTS users (
 INSERT INTO users(githubid, imageurl, username) VALUES
   ('18500240','https://avatars.githubusercontent.com/u/18500240?v=3', 'denesnori'),
   ('18164707', 'https://avatars.githubusercontent.com/u/18164707?v=3','RhodesPeter'),
-  ('17480329', 'https://avatars.githubusercontent.com/u/17480329?v=3', 'marko'),
   ('16775804','https://avatars.githubusercontent.com/u/16775804?v=3', 'Cleop'),
   ('16468314','https://avatars.githubusercontent.com/u/16468314?v=3', 'njsfield'),
-  ('15656538','https://avatars.githubusercontent.com/u/15656538?v=3','jsms90')
+  ('15656538','https://avatars.githubusercontent.com/u/15656538?v=3','jsms90'),
+  ('16781258', 'https://avatars.githubusercontent.com/u/16781258?v=3','lucymonie')
 ON CONFLICT DO NOTHING;
 
 COMMIT;

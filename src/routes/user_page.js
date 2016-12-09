a067b6b859357fd88dc4ec64fb5a942bde314351
denesnori <denes.nori@yahoo.com>
2016-12-09 00:17:05 +0000

    add velocity

--- src/routes/user_page.js
+++ src/routes/user_page.js
@@ -1,5 +1,6 @@
 const request = require('request');
 
+
 module.exports = {
   method:'GET',
   path:'/issues',
@@ -16,12 +17,25 @@ module.exports = {
         issue_number: a.number,
         title: a.title,
         state: a.state,
-        created_at: a.created_at,
-        closed_at: a.closed_at
+        created_at: new Date(a.created_at),
+        closed_at: new Date(a.closed_at),
+        velocity: (a.state === 'open'? null : velocity(a.closed_at,a.created_at))
       }
     });
     //reply(issueData);
     reply.view('user_issues', {issueData})
+
+    function velocity(endDate,startDate){
+      var end = new Date(endDate).getTime();
+      var start = new Date(startDate).getTime();
+      var milliseconds = end-start;
+      var seconds = (milliseconds / 1000) % 60 ;
+      var minutes =  Math.floor((milliseconds / (1000*60)) % 60);
+      var hours =  Math.floor((milliseconds / (1000*60*60)) % 24);
+      var days = Math.floor((milliseconds / (1000*60*60*24)) % 7);
+      var weeks = Math.floor(milliseconds / (1000*60*60*24*7));
+      return weeks+':'+days+':'+hours+':'+minutes+':'+seconds;
+    }
   })
 }
 };
